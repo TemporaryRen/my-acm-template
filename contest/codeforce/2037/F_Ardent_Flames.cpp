cebdd696a89faea2002230f4ac8c772b8abb6991
temporary <temporary.linshi.ren@gmail.com>
2024-12-19 20:08:54 +0800

    codeforce 2037

--- contest/codeforce/2037/F_Ardent_Flames.cpp
+++ contest/codeforce/2037/F_Ardent_Flames.cpp
@@ -7,23 +7,33 @@ ll h[MX],x[MX];
 ll n,m,k;
 void solve(){
     cin>>n>>m>>k;
-    forn(i,0,n) cin>>h[i];
-    forn(i,0,n) {cin>>x[i];x[i]--;}
-    ll l = 0LL,r = 1e14 + 5;
+    ll mmax = 0;
+    forn(i,0,n) {cin>>h[i];mmax = max(mmax,h[i]);}
+    forn(i,0,n) {cin>>x[i];}
+    ll limit = 1e9+1;
+    ll l = 0LL,r = limit;
     auto check = [](const int ak)->bool {
+        vector<array<int,2>> cnt;
         for(int i=0;i<n;i++){
-            int t = (h[i] + ak - 1) / ak;
+            ll t = (h[i] + ak - 1) / ak;
             if(t>m) continue;
+            cnt.push_back({x[i]-(m-t),1});
+            cnt.push_back({x[i]+(m-t)+1,-1});
         }
-        map<int,int> cnt;
-        return true;
+        sort(cnt.begin(),cnt.end());
+        ll total = 0LL;
+        for(auto [_,c] : cnt){
+            total += c;
+            if(total >= k) return true;
+        }
+        return false;
     };
     while(r - l > 1){
         ll mid = (r + l) / 2;
         if(check(mid)) r = mid;
         else l = mid;
     }
-    cout<< (r != 1e14 + 5 ? r : -1)<<"\n";
+    cout<< (r != limit ? r : -1)<<"\n";
 }
 
 
